a65e8387328b45c125dd23e19b7d4bd39e97e04c
kimchieater <jasonj19276@gmail.com>
2024-05-23 23:58:37 +1000

    updated

--- egoless/src/app/myWork/page.tsx
+++ egoless/src/app/myWork/page.tsx
@@ -2,7 +2,7 @@
 
 const MyWork = () =>{
   return(
-    <div id="myWork" className="h-screen bg-neutral-600">
+    <div id="myWork" className="h-screen w-screen bg-neutral-600">
       <div className="pt-[200px]">
         <h1>Project Title</h1>
       </div>
