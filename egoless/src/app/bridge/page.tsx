2c4473c07fdf2531a955d9e96b332fc3bdc17085
kimchieater <jasonj19276@gmail.com>
2024-05-24 03:07:38 +1000

    updated hero

--- egoless/src/app/bridge/page.tsx
+++ egoless/src/app/bridge/page.tsx
@@ -12,18 +12,31 @@ const Bridge = () => {
 
     const {scrollYProgress} = useScroll({
         target: targetRef,
-        offset: ["start 67%", 'end 40%'],
+        offset: ["50% 50%", '100% start'],
     });
-    const translateX = useTransform(scrollYProgress, [0, 1], ["0px", "400%"])
+
+    const translateX = useTransform(scrollYProgress, [0, 1], ["0px", "200%"])
+    const negativeTranslateX = useTransform(scrollYProgress, [0, 1], ["0px", "-200%"])
     const translateY = useTransform(scrollYProgress, [0, 1], ["0px", "-400%"])
+    const opacity = useTransform(scrollYProgress, [0, 1], [1, 0.5])
+
     const rotate = useTransform(scrollYProgress, [0, 1], ["0deg", "360deg"])
 
 
     return (
-    <section ref={targetRef} className="h-screen relative">
-        
-        
-        
+    <section ref={targetRef} className="h-screen relative border">
+        <motion.div style={{translateY: translateY, opacity, zIndex:"-20"}} className="absolute bottom-[-600px] left-[400px]">
+        <Image src="/hero3bg.png" alt="lol" width="1000" height="1000" 
+        ></Image></motion.div>
+        <motion.div className="absolute bottom-0 left-[-200px]" style={{translateX, opacity}}>
+        <Image src="/flowerjpeg1.png" alt="lol" width="400" height="1000"></Image></motion.div>
+        <motion.div className="absolute bottom-0 right-[-100px]" style={{translateX:negativeTranslateX, opacity}}>
+        <Image src="/flowerjpeg2.png" alt="lol" width="400" height="1000"></Image></motion.div>
+        <motion.div className="absolute bottom-0 right-[-300px]" style={{translateX:negativeTranslateX, opacity}}>
+        <Image src="/flowerjpeg3.png" alt="lol" width="400" height="1000" ></Image></motion.div>
+        <motion.div className="absolute bottom-[-200px] left-[-400px]" style={{translateX, opacity, zIndex:"-20"}}>
+        <Image src="/flowerjpeg4.png" alt="lol" width="400" height="1000" ></Image>
+        </motion.div>
     </section>
     )
 }
--- egoless/src/app/component/nav/page.tsx
+++ egoless/src/app/component/nav/page.tsx
@@ -5,9 +5,9 @@ import Link from "next/link"
 const Nav = () => {
   return(
     <div className="fixed flex justify-between z-10  font-light gap-10 w-[100%] px-10 py-5">
-      <Link href='/'  className="lg:text-[2rem] sm:text-[1rem]" id="/">HOME</Link>
-      <Link href='#myWork'  className="lg:text-[2rem] sm:text-[1rem]">MY WORK</Link>
-      <Link href='#aboutMe'  className="lg:text-[2rem] sm:text-[1rem]">ABOUT ME</Link>
+      <Link href='/'  className="lg:text-[2rem] sm:text-[1rem] hover:underline transition-all duration-75 " id="/">HOME</Link>
+      <Link href='#myWork'  className="lg:text-[2rem] sm:text-[1rem] ">MY WORK</Link>
+      <Link href='#aboutMe'  className="lg:text-[2rem] sm:text-[1rem] ">ABOUT ME</Link>
       <Link href='#contactMe' className="lg:text-[2rem] sm:text-[1rem]">CONTACT</Link>
     </div>
   )
