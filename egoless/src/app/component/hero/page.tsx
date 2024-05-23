51f939d4eaee0c7a859615595a8d6a3401809b8a
kimchieater <jasonj19276@gmail.com>
2024-05-23 15:19:28 +1000

    finished hero

--- egoless/src/app/component/hero/page.tsx
+++ egoless/src/app/component/hero/page.tsx
@@ -1,8 +1,10 @@
 'use client'
+import { motion } from 'framer-motion';
 import Image from 'next/image'
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const Hero = () => {
+
     // interface 
 
     interface MousePosition {
@@ -10,10 +12,12 @@ const Hero = () => {
     y: number;
     }
 
-interface ImagePosition {
+    interface ImagePosition {
     left: number;
     top: number;
     }
+
+
     //state
 
     //for mouse coordinates
@@ -47,8 +51,13 @@ interface ImagePosition {
     };
 
     const spawnImage = (x:number, y:number): void => {
+        const imageKey = imageNumber;
+        
         const newImage = (
-            <div key={imageNumber} style={{
+            <motion.div initial={{opacity:1, scale:2}}
+            animate={{opacity:0, scale:.1}}
+            transition={{duration:.5}}
+            key={imageKey} style={{
                 top: `${y}px`,
                 left: `${x}px`,
                 position: "absolute",
@@ -58,15 +67,23 @@ interface ImagePosition {
                 
             }}>
                 <Image src={`/image-${imageNumber}.avif`} alt={`image ${imageNumber}`} width={500} height={100} className={"object-cover"} />
-            </div>
+            </motion.div>
         );
         setImages(prevImages => {
-            if (prevImages.length >= 36) {
+            if (prevImages.length >= 12) {
                 return [newImage];
             } else {
                 return [...prevImages, newImage];
             }
         });
+        setTimeout(() => {
+            const imageElement = document.getElementById(`image-${imageKey}`);
+            if (imageElement) {
+                imageElement.style.opacity = '0';
+                imageElement.style.transform = 'translate(-50%, -50%) scale(0.2)';
+            }
+        }, 500);
+
         setImageNumber((prevNumber: number) => (prevNumber === 12 ? 1 : prevNumber + 1));
     };
     
@@ -81,7 +98,7 @@ interface ImagePosition {
                 </div>            
             </div>
             
-            <h1 className="absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] text-[4rem] font-black z-1">
+            <h1 className="absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] text-[4rem] font-black z-1 bg-lime-300 px-2 py-1">
                 EGOLESS CODING
             </h1>
         </div>
