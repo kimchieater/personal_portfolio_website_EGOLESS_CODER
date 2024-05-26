c5cd2ee0afee38d612626d3eb2bdb63ec506f1be
kimchieater <jasonj19276@gmail.com>
2024-05-26 13:33:44 +1000

    Update page.tsx

--- egoless/src/app/component/inMy/page.tsx
+++ egoless/src/app/component/inMy/page.tsx
@@ -14,24 +14,19 @@ const InMy = () =>{
 
   })
 
-  const x = useTransform(scrollYProgress, [0, 1], ["100%", "-200%"])
+  const x = useTransform(scrollYProgress, [0, 1], ["130%", "-200%"])
 
   return(
       <motion.div style={{x}} className="sticky py-10 top-0 flex items-center overflow-hidden gap-5">
-        <div className="w-[400px] h-[400px] bg-white"></div>
-        <div className="w-[400px] h-[400px] bg-white"></div>
-        <div className="w-[400px] h-[400px] bg-white"></div>
-        <div className="w-[400px] h-[400px] bg-white"></div>
-        <div className="w-[400px] h-[400px] bg-white"></div>
-        <div className="w-[400px] h-[400px] bg-white"></div>
-        <div className="w-[400px] h-[400px] bg-white"></div>
-        <div className="w-[400px] h-[400px] bg-white"></div>
-        <div className="w-[400px] h-[400px] bg-white"></div>
-        <div className="w-[400px] h-[400px] bg-white"></div>
-        <div className="w-[400px] h-[400px] bg-white"></div>
-        <div className="w-[400px] h-[400px] bg-white"></div>
-        <div className="w-[400px] h-[400px] bg-white"></div>
-        <div className="w-[400px] h-[400px] bg-white"></div>
+        <div className="w-[50%] h-[90vh] bg-white"></div>
+        <div className="w-[50%] h-[90vh] bg-white"></div>
+        <div className="w-[50%] h-[90vh] bg-white"></div>
+        <div className="w-[50%] h-[90vh] bg-white"></div>
+        <div className="w-[50%] h-[90vh] bg-white"></div>
+        <div className="w-[50%] h-[90vh] bg-white"></div>
+        
+        
+        
       </motion.div>
   )
 }
