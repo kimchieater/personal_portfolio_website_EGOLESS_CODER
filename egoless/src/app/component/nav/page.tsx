cc8f39f7a94e32d993041f1192359c95eb3940e8
kimchieater <jasonj19276@gmail.com>
2024-05-26 11:07:36 +1000

    careated a burger menu

--- egoless/src/app/component/nav/page.tsx
+++ egoless/src/app/component/nav/page.tsx
@@ -4,17 +4,20 @@ import Link from "next/link"
 
 const Nav = () => {
   return(
+    <div>
+      <div className=" fixed flex flex-col gap-2 justify-center mx-auto left-[50%] translate-x-[-50%] py-5 md:hidden">
+        <div className="w-[50px] h-[10px] bg-black"></div>
+        <div className="w-[50px] h-[10px] bg-black"></div>
+        <div className="w-[50px] h-[10px] bg-black"></div>
+      </div>
     <div className="fixed hidden justify-between z-10  font-light gap-10 w-[100%] px-10 py-5 md:flex">
       {/* hamburger drop down */}
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+      
       <Link href='/'  className="lg:text-[2rem] sm:text-[1rem] hover:underline transition-all duration-75 " id="/">HOME</Link>
       <Link href='#myWork'  className="lg:text-[2rem] sm:text-[1rem] ">MY WORK</Link>
       <Link href='#aboutMe'  className="lg:text-[2rem] sm:text-[1rem] ">ABOUT ME</Link>
       <Link href='#contactMe' className="lg:text-[2rem] sm:text-[1rem]">CONTACT</Link>
+    </div>
     </div>
   )
   
