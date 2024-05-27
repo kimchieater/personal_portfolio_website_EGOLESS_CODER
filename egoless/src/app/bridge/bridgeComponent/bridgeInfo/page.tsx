d5e92d9ab97294b7974bb09004c4555c640230cd
kimchieater <jasonj19276@gmail.com>
2024-05-27 23:18:56 +1000

    Update page.tsx

--- egoless/src/app/bridge/bridgeComponent/bridgeInfo/page.tsx
+++ egoless/src/app/bridge/bridgeComponent/bridgeInfo/page.tsx
@@ -13,46 +13,47 @@ const BridgeInfo = () => {
 });
 
     return (
-
-        <motion.div ref={scopeRef} className="text-left absolute bottom-[300px] left-[50%] translate-x-[-50%] h-[500px] text-neutral-300 text-[2.5rem] font-black flex flex-col sm:gap-5">
+        <div className="relative ">
+        <motion.div ref={scopeRef} className="text-left absolute top-[100px] left-[50%] translate-x-[-50%] h-[350px] text-neutral-300 font-black flex flex-col sm:gap-[5rem] text-[1rem]  xl:text-[1.7rem] lg:text-[1.3rem] md:text-[1.2rem] sm:text-[1rem]" >
           
             <motion.p 
             initial={{opacity:0, x:"-100px"}}
-            whileInView={{opacity:1, x:"-50px"}}
-            transition={{duration:2}}
+            whileInView={{opacity:1, x:"0px"}}
+            transition={{duration:1.2}}
             viewport={{
               margin:"-300px",
               once:true
             }}
             ><span className="text-lime-300">Developing</span> UX/UI</motion.p>
             <motion.p
-            initial={{opacity:0, x:"-50px"}} 
+            initial={{opacity:0, x:"-100px"}} 
             whileInView={{opacity:1, x:"0px"}}
-            transition={{duration:2}}
+            transition={{duration:1.2}}
             viewport={{
               margin:"-300px",
               once:true
             }}
             > with <span className="text-lime-300">Creative/Interactive</span> Ideas</motion.p>
             <motion.p
-            initial={{opacity:0, x:"50px"}} 
-            whileInView={{opacity:1, x:"100px"}}
-            transition={{duration:2}}
+            initial={{opacity:0, x:"-100px"}} 
+            whileInView={{opacity:1, x:"0px"}}
+            transition={{duration:1.2}}
             viewport={{
               margin:"-300px",
               once:true
             }}
             >Making <span className="text-lime-300">Dynamic</span> websites</motion.p>
             <motion.p 
-            initial={{opacity:0, x:"100px"}} 
-            whileInView={{opacity:1, x:"150px"}}
-            transition={{duration:2}}
+            initial={{opacity:0, x:"-100px"}} 
+            whileInView={{opacity:1, x:"0px"}}
+            transition={{duration:1.2}}
             viewport={{
               margin:"-300px",
               once:true
             }}
             > Strive on, <span className="text-lime-300">untiringly.</span></motion.p>
         </motion.div>
+        </div>
     )
 }
 
