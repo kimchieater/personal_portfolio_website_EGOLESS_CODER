f65fc526ab9ed2d1350213288271d2973b6a0d04
kimchieater <jasonj19276@gmail.com>
2024-06-05 22:00:08 +1000

    awd

--- egoless/src/app/contactMe/page.tsx
+++ egoless/src/app/contactMe/page.tsx
@@ -1,9 +1,15 @@
+'use client'
+
 import { motion, useScroll } from 'framer-motion';
-import React from 'react';
+import React, { useRef } from 'react';
 
 
 
 const ContactMe = () => {
+
+
+
+
   return (
     <div id="contactMe" className="h-screen bg-neutral-500 text-white flex justify-center items-center">
       <ContactTable></ContactTable>
@@ -15,8 +21,22 @@ export default ContactMe;
 
 
 const ContactTable = ()=>{
+
+  const targetRef = useRef<HTMLDivElement>(null);
+
+
   return(
-    <motion.div className=" h-[80%] w-[80%] flex flex-col justify-center items-center gap-5">
+    <motion.div ref={targetRef} 
+    initial={{opacity:0, x:"-100px"}}
+    whileInView={{opacity:1, x:"0px"}}
+    transition={{duration:1}}
+    viewport={{
+      margin:"-100px",
+      once:true
+    }}
+    
+    
+    className=" h-[80%] w-[80%] flex flex-col justify-center items-center gap-5">
 
       
     <div className="flex flex-col items-center justify-center p-5">
@@ -27,25 +47,29 @@ const ContactTable = ()=>{
 
       <div className="flex flex-col items-center justify-center md:flex-row gap-5 w-[400px]">
         <label className="flex flex-auto flex-col items-stretch">
-          First Name<input type="text" className="flex-1 text-neutral-700"></input>
+          First Name
+          <input type="text" className="flex-1 text-neutral-200 rounded-lg py-2 px-3 bg-neutral-400"></input>
         </label>
         <label className="flex flex-col flex-auto items-stretch">
-          Last Name<input type="text" className="flex-1 text-neutral-700"></input>
+          Last Name
+          <input type="text" className="flex-1 text-neutral-200 rounded-lg py-2 px-3 bg-neutral-400"></input>
         </label>
       </div>
 
       <div className="flex flex-col items-center justify-center md:flex-row gap-5 w-[400px]">
         <label className="flex flex-col items-start">
-          Email<input type="text" className=" text-neutral-700"></input>
+          Email
+          <input type="email" className=" flex-1 text-neutral-200 rounded-lg py-2 px-3 bg-neutral-400"></input>
         </label>
         <label className="flex flex-col items-start">
-          Phone Number<input type="text" className="text-neutral-700"></input>
+          Phone Number 
+          <input type="text" className="text-neutral-200 rounded-lg bg-neutral-400 py-2 px-3"></input>
         </label>
       </div>
 
       
         <label className="flex flex-col w-[420px]  text-center">Message
-          <textarea className=" text-neutral-700"></textarea>
+          <textarea className=" text-neutral-200 rounded-lg bg-neutral-400 py-2 px-3"></textarea>
         </label>
       
 
